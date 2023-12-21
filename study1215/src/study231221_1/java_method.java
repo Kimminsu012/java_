107cbb343aff057978c2996503d82433310a4485
kimminsu012 <asdfg002269@gmail.com>
2023-12-21 13:39:49 +0900

    method 수업

--- study1215/src/study231221_1/java_method.java
+++ study1215/src/study231221_1/java_method.java
@@ -1,10 +1,70 @@
 package study231221_1;
 
+import java.util.Scanner;
+
 public class java_method {
 
+	static Scanner sc = new Scanner(System.in);
+	
+	static void list_print(music[] mylist) {
+		for( music obj : mylist ) {
+			System.out.println(obj);			
+		}
+	}
+	
+	static void song_search(String keyword , music[] mylist) {
+		
+		boolean issong = false;
+		for( music obj : mylist) {
+			if( obj.singer.equals(keyword) || obj.title.equals(keyword) ) {
+				System.out.println(obj);
+				issong = true;
+			}
+		}
+		if( !issong )
+			System.out.println("검색결과없음.");
+	}	
+	
+//	static void song_search( music[] mylist , String sear) { // 가수 또는 노래제목 으로 검색
+//		boolean isMylist = false;
+//		
+//		for(int i=0; i<mylist.length; i++) {
+//			if(mylist[i].singer.equals(sear) || mylist[i].title.equals(sear) ) {
+//				System.out.println(mylist[i]);
+//				isMylist = true;
+//			}
+//		}
+//		if( !isMylist )
+//			System.out.println("일치하는 노래가 없습니다.");
+//	}
+	
 	public static void main(String[] args) {
 		
-
+		music[] mylist = new music[] {
+				new music("잔나비","Made In Christmas",312)
+				,new music("버블 사운드","그 남자의 크리스마스",339)
+				,new music("버블 사운드","그 여자의 크리스마스",339)
+				,new music("트와이스","Merry & Happy",329)
+				,new music("아이유","미리 메리 크리스마스",437)
+				,new music("성시경,박효신,이석훈,서인국,빅스","크리스마스니까",349)
+				,new music("탑현","호랑수월가",441)
+				,new music("조정석","아로하",405)
+				,new music("다비치","화이트",414)
+				,new music("써니힐","추워지니",424)	
+		};
+		System.out.println("찾는 노래의 제목 혹은 가수를 적어주세요.");
+//		String sear = sc.nextLine();
+		
+		String keyword = sc.nextLine();
+		song_search(keyword,mylist);
+		
+		
+//		song_search(mylist,sear);
+		
+		
+		
+		
+		
 	}
 
 }
---
+++ study1215/src/study231221_1/music.java
@@ -0,0 +1,39 @@
+package study231221_1;
+
+public class music {
+
+	static int count=0; // 클래스변수 , 명시적초기화로 0 저장
+	
+	String singer; // 가수
+	String title; // 제목
+	int playing_time; // 재생시간
+	
+	music(){} // 기본 생성자 메서드
+	music(String 가수 , String 제목){ // 매개변수가 있는 생성자 메서드
+		this.singer=가수;
+		this.title=제목;
+	}
+	music(String singer , String title , int time){ // 매개변수가 있는 생성자 메서드
+		this(singer,title); // 저장 되어있는 singer,title의 메서드를 실행 (위에 만들어진 생성자 메서드 실행)
+		this.playing_time=time;
+	}
+	
+	@Override
+	public String toString() {
+		return "가수 : "+singer+" - "+title+" ("+transform()+")";
+	}
+	
+	String transform() { // return이 있어 void 타입이 아닌 그게 맞는 타입으로 해야한다.
+		int min = playing_time/100; // 정수/정수 -> 정수를 이용
+		int sec = playing_time%100; // 나머지를 가져오는 값
+		
+		return min+":"+sec; 
+	}
+	
+}
+
+// 숫자를 문자열로 변환 하여 원하는 숫자를 빼오는 방법 
+//	int a = 456;
+//	integer.toString(a).charAt(0);//substring(0,1);
+
+
