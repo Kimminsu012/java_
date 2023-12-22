bbfe38f539d0fcf8cdea84c214d6993417fd8182
kimminsu012 <asdfg002269@gmail.com>
2023-12-22 09:30:14 +0900

    dd

---
+++ study1215/src/study231222/buyer.java
@@ -0,0 +1,5 @@
+package study231222;
+
+public class buyer {
+
+}
---
+++ study1215/src/study231222/mart.java
@@ -0,0 +1,26 @@
+package study231222;
+
+import java.text.DecimalFormat;
+
+public class mart {
+	String product_name; // 제품명
+	int price; // 제품가격
+	String category; // 제품 분류
+	int quan; // 수량
+	
+	mart(){}
+	mart(String name, int price , String category , int quan){
+		this.product_name = name;
+		this.price = price;
+		this.category = category;
+		this.quan = quan;
+	}
+	
+	@Override
+	public String toString(){
+		String price_ = Integer.toString(price).replaceAll( "\\B(?=(\\d{3})+(?!\\d))" , "," );
+		DecimalFormat df = new DecimalFormat("###,###"); // 문자열 형식을 지정 #은 숫자를 표현
+		
+		return product_name + " - " + price_ + "원 (" + quan + "개)";
+	}
+}
