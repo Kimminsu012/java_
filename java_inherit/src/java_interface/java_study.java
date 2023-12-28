90d7ad2fcd9aa9ba90b0264e204709a52ba45e0f
kimminsu012 <asdfg002269@gmail.com>
2023-12-28 11:51:38 +0900

    interface 수업

---
+++ java_inherit/src/java_interface/java_study.java
@@ -0,0 +1,54 @@
+package java_interface;
+
+public class java_study {
+
+	public static void main(String[] args) {
+		
+		
+		
+//		unit m1 = new marine();
+//		unit h1 = new hydra();
+//		
+//		m1.attack();
+//		h1.attack();
+		
+		
+//		marine m1 = new marine();
+//		hydra h1 = new hydra();
+//		
+//		m1.move();
+//		m1.attack();
+//		
+//		h1.move();
+//		h1.attack();
+//		
+//		m1.dead();
+		
+		
+		
+	}
+
+}
+
+
+// 인터페이스
+//		   - 추상클래스의 일종
+//		   - interface 이름 (생성할 때의 모습)
+//		   - 변수를 가질 수 없다, 메서드 구현이 불가 (class와 다른점)
+//		   - 기본적으로 메서드는 추상 메서드 이다.
+//		   - static 변수 , 메서드는 생성 가능
+//		   - final 변수 ( 상수 ) 생성 가능
+
+//		   - 클래스들이 구현해야 하는 동작( 메서드 )를 지정 하는데 사용
+
+
+
+
+
+
+
+
+
+
+
+
