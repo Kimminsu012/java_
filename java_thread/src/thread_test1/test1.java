6a53f4a11cbbe8d2aef8cbd622c4143cb692309a
kimminsu012 <asdfg002269@gmail.com>
2024-01-09 17:30:34 +0900

    쓰레드 수업

--- java_thread/src/thread_test1/test1.java
+++ java_thread/src/thread_test1/test1.java
@@ -3,8 +3,72 @@ package thread_test1;
 public class test1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		myThread th1 = new myThread();
+		th1.start(); // 쓰레드실행
+		for(int i=1; i<=10; i++) {
+			System.out.println("main메서드 "+i);
+			try {
+				Thread.sleep(1000);
+			}catch(Exception e) {
+				System.out.println("실패");
+			}
+		}
+		
+		Runnable th2 = new thread2();
+		Thread th22 = new Thread(th2);
+		th22.start();
+		
 	}
 
 }
+
+class thread2 implements Runnable { // 인터페이스 사용
+
+	@Override
+	public void run() { // Runnable 인터페이스에 있는 메서드
+		System.out.println("나는 인터페이스로 작업한 쓰레드");
+	}
+	
+}
+
+
+class myThread extends Thread{ // 쓰레드 사용
+	@Override
+	public void run() {
+		for(int i=1; i<=10; i++) {
+			System.out.println("나는 쓰레드");
+			try {
+				Thread.sleep(1000);
+			}catch(Exception e) {
+				System.out.println("실패");
+			}
+		}
+	}
+}
+
+
+
+// 프로세스 - cpu에의해 실행중인 프로그램
+// 		    프로그램에 대한 정보를 가지고 있다.
+// 쓰레드 - 프로세스 안에서 실질적으로 실행 되는 단위
+
+// 단일쓰레드 - 하나의 작업만 이루어 지는 프로그램, 순차적 실행
+// 멀티쓰레드 - 여러 작업을 동시에 실행 시키는 프로그램, 비동기
+//		      멀티쓰레드 사용 시 공유 자원에대해 주의 해야한다.
+// 공유자원 - 
+
+// 쓰레드 관련 메서드
+// run() : 쓰레드의 실행코드가 작성되는 메서드 - 쓰레드 stack first input
+// start() : 쓰레드가 시작되도록 요청하는 메서드 - run 메서드를 호출한다.
+// sleep(밀리세컨드) : 지정한 시간동안 현재 쓰레드를 일시 중지 시킨다.
+// join() : 현재 쓰레드가 끝날때까지 기다린다.
+// yield() : 현재 쓰레드의 실행 시간을 다른 쓰레드에게 양보한다.
+
+// 데몬 스레드 - 주 쓰레드의 작업을 보조 하는 쓰레드
+//			   주 쓰레드가 종료 되면 강제적으로 종료된다.
+
+
+
+
+
+
