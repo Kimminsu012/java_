ee7f05e410c96be655bbf42040e37f170f25d08f
kimminsu012 <asdfg002269@gmail.com>
2024-01-10 10:28:09 +0900

    쓰레드 수업

--- java_thread/src/thread_test1/test5.java
+++ java_thread/src/thread_test1/test5.java
@@ -1,7 +1,5 @@
 package thread_test1;
 
-import java.util.Scanner;
-
 public class test5 {
 
 	public static void main(String[] args) {
@@ -9,6 +7,13 @@ public class test5 {
 		
 		Thread th1 = new Thread(r);
 		Thread th2 = new Thread(r);
+		
+		th1.setName("1번");
+		th2.setName("2번");
+		
+		th1.start();
+		th2.start();
+		
 	}
 
 }
@@ -21,8 +26,16 @@ class Bank{
 	public int getMyMoney() {
 		return myMoney;
 	}
-	public boolean withdraw(int money) {
-		Scanner sc = new Scanner(System.in);
+	// 쓰레드 동기화 임계영역 설정 과 락 이 필요하다.
+	// 임계영역 - 둘 이상의 쓰레드가 동시에 접근해서는 안되는 코드 부분
+	// 쓰레드가 임계영역에 접근하면 해당 쓰레드는 락을 가지게 된다.
+	// 해당 쓰레드가 락을 바납하기 전까지는 다른 쓰레드는 임계영역에 접근 하지 못 한다.
+	// 메서드의 반환 타입 앞에 synchronized를 붙이거나
+	// 특정코드를 블럭 지정 한다.
+	// synchronized(this){코드}
+	
+	// withdraw 메서드 임계영역 설정
+	public synchronized boolean withdraw(int money) {
 		// 게좌 잔고가 출금할 금액보다 크거나 같으면 출금 가능
 		if(myMoney >= money) {
 			try { Thread.sleep(1000);}
@@ -41,7 +54,18 @@ class myApp implements Runnable{
 	
 	@Override
 	public void run() {
+		while( my.getMyMoney() > 0 ) {
+			int money = (int)(Math.random()*5+1)*10000;
+			
+			boolean fail = my.withdraw(money);
 		
+			String thName = Thread.currentThread().getName();
+			if( fail ) {
+			System.out.printf( "쓰레드 : %s 출금 : %d원 잔액 : %d원\n",thName,money,my.getMyMoney() );
+			}else {
+				System.out.println(thName + "잔액부족");
+			}
+		}
 	}
 }
 
