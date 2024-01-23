17cc71c4e0a7b4d3fb54df53c59f632dddb2fc67
kimminsu012 <asdfg002269@gmail.com>
2024-01-23 17:30:47 +0900

    web model2

--- model2/src/main/java/control/member_control.java
+++ model2/src/main/java/control/member_control.java
@@ -1,6 +1,8 @@
 package control;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,12 +10,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import service.member_service;
+
 /**
  * Servlet implementation class member_control
  */
-@WebServlet("/member") // 원하는 주소의 이름으로 변경하여 사용이 가능하다.
+@WebServlet("/members") // 원하는 주소의 이름으로 변경하여 사용이 가능하다.
 public class member_control extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private member_service ms = new member_service();
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -35,15 +40,7 @@ public class member_control extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		
-		String cmd = request.getParameter("cmd");
-		if( cmd.equals("signup") ) {
-			response.sendRedirect("/member/signup.jsp");
-		}
-		
-//		response.getWriter().append("Served at: ").append(request.getContextPath());
-//		response.getWriter().append("my site ㅘㅘㅘㅘ");
-		
+		process(request, response);
 	}
 
 	/**
@@ -51,7 +48,89 @@ public class member_control extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		process(request, response);
+	}
+	
+	// GET, POST 요청에 대한 일괄처리 메서드
+	private void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		// 서버는 대부분 사용자의 주소요청에 동작한다. 주소요청이 없으면 대부분 동작하지않는다.
+		// 사용자가 요청한 주소를 보고 그에 맞는 응답 페이지 제공이나 값을 제공한다.
+		// 서블렛은 사용자의 요청을 목적에 맞게 처리 하기위한 클래스이다.
+		// 서버 대신해서 요청을 처리해주는 역할을 한다.
+		request.setCharacterEncoding("UTF-8");
+
+		
+		String cmd = request.getParameter("cmd"); // 요청 주소의 cmd 파라미터 값 가져오기
+		String view = "/"; // 사용자에게 제공할 기본 페이지
+		
+		if( cmd.equals("signup") ) { // 요청 파라미터가 signup일 경우 회원가입 페이지를 요청한거
+			request.setAttribute("prt", "member/signup");
+			
+			request.setAttribute( "emailList", ms.emailList() );
+			// 회원가입 페이지를 제공하기위해 request객체를 이요한것일뿐 반드시 이와 같은 방법이 아니여도 된다.
+			// 사용자 요청에 의한 페이지 제공은 정적페이지(.html, .jsp 등) 그대로 제공할수도 있고
+			// 위와 같이 include 방식으로 제공 할 수 있다.
+		}else if( cmd.equals("signSave") ) {
+			view = ms.insert(request, response);
+		}
+		
+		if( view != null ) {
+		RequestDispatcher rds = request.getRequestDispatcher(view); // 제공할 페이지
+		rds.forward(request, response);
+		// 기존의 사용자 브라우저의 내용을 교체하는 방식
+		// 주소는 변경되지않는다.
+		}
+		
 	}
 
 }
+
+
+
+
+
+
+/*
+ * RequestDispatcher (페이지 유지 후 내용만 변경)
+ * 	- 클라이언트의 요청이 들어오면 원하는 값으로 요청을 넘기거나
+ * 	  특정 값에 처리를 요청하고 처리결과를 얻어오는 기능
+ * 	  
+ * 	  서블렛에 의해서 사용자가 요청한 페이지를 제공하는 방법으로
+ * 	  response.sendRedirect가 있는데 사용자의 브라우저 주소를 변경하는 방법이라서
+ * 	  다시 한번 재요청이 들어온다.
+ * 	  
+ * 	  주소에 대해 요청을 한번 한 후 해당 주소의 정보를 받게 되면 그 주소의 html같은 내용들을 다시 재 요청 하는 구조
+ * 
+ * 
+ * 	  dispatcher를 사용하면 요청에 대한 페이지를 forward 또는 include하여 제공한다.
+ *    
+ *    주소에 대해 요청을 하면 해당 주소의 내용도 한번에 받을 수 있다. (재요청 없음)
+ * 
+ * 	  
+ * 
+ * 	  MVC 패턴
+ * 	  M : Model - 데이터 저장 객체
+ * 	  V : View - 클라이언트에게 제공하는 페이지
+ * 	  C : Controller - 클라이언트의 요청을 처리
+ * 
+ * 	  Control(서블렛) <--> Service <--> Database
+ * 
+ * 	  DAO - data access object (데이터 베이스에 접근하기 위한 객체)
+ * 	  DTO - data transfer object (데이터를 저장해두는 객체)
+ * 	  
+ * 	  VO - value object ( 데이터 저장 객체 - 읽기전용 )
+ * 	  Entity - 
+ * 
+ */
+
+
+
+
+
+
+
+
+
+
+
--- model2/src/main/webapp/member/signup.jsp
+++ model2/src/main/webapp/member/signup.jsp
@@ -1,6 +1,49 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 
-<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+${ emailList[0] }
+
+
+
+<div id="signup_wrap"> <!-- enctype - 문서 이미지 등등 업로드를 할려면 꼭 필요하다. --> 
+	<form id="signupFm" method="post" action="/members"> 
+		<input type="hidden" name="cmd" value="signSave">
+		<div class="myphoto">
+			<div class="photo"></div>
+			<input type="file" name="face" id="face">
+			<label for="face">내 사진 업로드</label>
+		</div>
+		
+		<div class="input_data">
+			<label for="email">E-MAIL</label>
+			<input type="email" name="email" id="email">
+			<span class="email_msg"></span>
+		</div>
+		<div class="input_data">
+			<label for="pin">PassWord</label>
+			<input type="password" name="pin" id="pin">
+		</div>
+		<div class="input_data">
+			<label for="pin2">Check PassWord</label>
+			<input type="password" id="pin2">
+		</div>
+		<div class="input_data">
+			<label for="name">Name</label>
+			<input type="text" name="name" id="name">
+		</div>
+		<div class="input_data">
+			<label for="tel">Tel.</label>
+			<input type="text" name="tel" id="tel">
+		</div>
+		
+		<div class="signBt">
+			<button type="button" id="bt" class="active inactive" disabled>Sign</button>
+		</div>
+		
+	</form>
+</div>
+
+
+
 
 
